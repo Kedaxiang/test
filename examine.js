e810b5b3b8cde548b9fdb83ba5d57e29d2aaa72f
wangyanhai <826107601@qq.com>
2019-03-29 11:39:26 +0800

    examine.js

--- examine.js
+++ examine.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function(){                     //窗口加载完成后执行function{}这个函数
     var tab = document.getElementById("tab");
     var lis = tab.getElementsByTagName('li');
     var cha = document.getElementsByClassName('change');
@@ -9,8 +9,16 @@ window.onload = function(){
             cha[j].style.display = "none";
             lis[j].classList.remove("activeTab");
             }
-        cha[this.num].style.display = 'block';
+        cha[this.num].style.display = 'block';  //this用于指定对象，调用num
         lis[this.num].classList.add("activeTab");
         }
+    }
+}
+window.onload = function(){
+    var oBtn = document.getElementById('login');
+    oBtn.onclick = show;
+    function show(){
+       prompt('username');
+       prompt('passport');
     }
 }
